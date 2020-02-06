a55b1af6ec157fa77752d801ae5aceb135fe4e8a
Gabriel <isporck0@gmail.com>
2020-02-06 11:52:45 -0300

    comit

--- main.js
+++ main.js
@@ -12,3 +12,27 @@ const instancesDropdown = M.Dropdown.init(elemsDropdown, {
 });
 elemensSidenav = document.querySelectorAll(".sidenav");
 const instacesSidenav = M.Sidenav.init(elemensSidenav);
+
+// Modal
+const elemsModal = document.querySelectorAll(".modal");
+const instancesModal = M.Modal.init(elemsModal);
+
+// Tooltip
+const elemsTooltip = document.querySelectorAll(".tooltipped");
+const instaceTooltpi = M.Tooltip.init(elemsTooltip,{
+    html: "olha essa dica!",
+    position: "bottom"
+});
+
+// Toast
+const elemnsToast = document.querySelector("#Toast");
+const instanceToast = () => {
+    M.toast( {
+        html: "Também sou uma notificação",
+        classes: "rounded "
+    })
+}
+
+elemnsToast.addEventListener("click", () => {
+    instanceToast();
+});
